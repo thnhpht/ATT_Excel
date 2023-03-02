939b507ad7737eac1c838008fa09daf82107a786
thnhpht <thnhpht1509@gmail.com>
2023-03-02 16:35:28 +0700

    clean code

--- time_attendance.py
+++ time_attendance.py
@@ -1,68 +1,32 @@
 import openpyxl
 import json
-from zk import ZK, const
+from zk import ZK
 import datetime
 from datetime import timedelta
 
-# Mở file config
-f = open("config.json", encoding="utf-8")
-  
-# Trả về dạng dictionary
-config = json.load(f)
 
-# Lặp và kết nối các máy chấm công từ file config
-attendances = []
-
-for x in config["time_attendance"]:
-    conn = None
-    zk = ZK(x["ip"], port=x["port"])
-    try:
-        conn = zk.connect()
-        attendances += conn.get_attendance()
-    except:
-        print("Kiểm tra lại máy chấm công số seri:", x["id"])
-        exit()
-
-# Lấy thông tin tất cả người dùng trong máy
-users = conn.get_users()
-
-# Lấy số phút cần trừ ra trước giờ chấm công
-minute_must_ci = config["minute"]
-
-# Lấy on duty, off duty
-list_on_duty = []
-list_off_duty = []
-
-for shift in config["shift"]:
-    list_on_duty.append(shift["work_start"])
-    list_off_duty.append(shift["work_end"])
-
-# Đóng file config
-f.close()
-
-# Hàm đưa dữ liệu chấm công vào mảng kiểu datetime theo userid
-def push_data_into_array(user_id):
+# Hàm lấy thời gian chấm công của người dùng trong khoảng thời gian theo userid
+def get_attendances_of_user(user_id, attendances, date_from, date_to):
     res = []
     del_list = []
-    # Lặp tất cả dữ liệu chấm công
+    # Lặp và lấy tất cả thời gian chấm công đúng userid nằm trong khoảng thời gian và xóa đi
     for attendance in attendances:
-        # Đưa dữ liệu không nằm trong khoảng thời gian vào mảng để xóa
         if not datetime_in_range(date_from, date_to, attendance.timestamp):
             del_list.append(attendance)
             continue
-        # Nếu đúng userid lấy datetime và đưa vào mảng
         if attendance.user_id == user_id:
             date_time = attendance.timestamp
             res.append(date_time)
             # Đưa vào mảng để xóa 
             del_list.append(attendance)
-    for x in del_list:
-        attendances.remove(x)
-    # Trả về mảng datetime được sắp xếp
+    for del_item in del_list:
+        attendances.remove(del_item)
+    # Trả về thời gian chấm công đã được sắp xếp
     return sorted(res)
 
-# Hàm lấy các ngày từ khoảng from to
-def get_date():
+
+# Hàm lấy tất cả các ngày từ khoảng thời gian bắt đầu - kết thúc
+def get_date(date_from, date_to):
     res = []
     current = date_from
 
@@ -71,18 +35,14 @@ def get_date():
         current += timedelta(days=1)
     return res
 
-# Hàm kiểm tra trong khoảng thời gian
+
+# Hàm kiểm tra ngày đang xét có nằm trong khoảng thời gian, nếu đúng trả về true ngược lại trả về false
 def datetime_in_range(start, end, current):
     return start <= current <= end
 
-# Hàm lấy timetable
-def check_sunday(current):
-    if current.strftime("%A") == "Sunday":
-        return "CN"
-    return "HC"
 
 # Hàm lấy giờ chấm công vào
-def get_clock_in(current):
+def get_clock_in(current, on_duty, off_duty, minute_must_ci):
     global list_datetime
     # Lấy thời gian on duty, off duty
     time_on_duty = datetime.time(int(on_duty.split(":")[0]), int(on_duty.split(":")[1]))
@@ -98,15 +58,15 @@ def get_clock_in(current):
         end = current + timedelta(hours=hour_off_duty, minutes=minute_off_duty) + timedelta(days=1)
     else:
         end = current + timedelta(hours=hour_off_duty, minutes=minute_off_duty)
-    # Lặp tất cả datetime trong mảng
-    for id, current in enumerate(list_datetime):
-        # Nếu current đúng trong khoảng thời gian start end trả về current
+    # Lặp và lấy đúng thời gian chấm công vào, không có trả về None
+    for i, current in enumerate(list_datetime):
         if datetime_in_range(start, end, current):
-            list_datetime = list_datetime[id+1:]
+            list_datetime = list_datetime[i + 1:]
             return str(current)
 
+
 # Hàm lấy giờ chấm công ra
-def get_clock_out(current):
+def get_clock_out(current, on_duty, minute_must_ci):
     global list_datetime
     res = ""
     flag = False
@@ -115,83 +75,133 @@ def get_clock_out(current):
     minute_on_duty = int(on_duty.split(":")[1])
     # Lấy thời gian bắt đầu và kết thúc
     start = current + timedelta(hours=hour_on_duty, minutes=minute_on_duty)
-    end = current + timedelta(hours=hour_on_duty, minutes=minute_on_duty) + timedelta(days=1) - timedelta(minutes=minute_must_ci, seconds=1)
-    # Lặp tất cả datetime trong mảng
-    for id, current in enumerate(list_datetime):
-        # Nếu current đúng trong khoảng thời gian start end trả về current
+    end = current + timedelta(hours=hour_on_duty, minutes=minute_on_duty) + timedelta(days=1) - timedelta(
+        minutes=minute_must_ci, seconds=1)
+    # Lặp và lấy đúng thời gian chấm công ra, không có trả về None
+    for i, current in enumerate(list_datetime):
         if datetime_in_range(start, end, current):
             res = current
             flag = True
             continue
-        if flag == True:
-            list_datetime = list_datetime[id:]
+        if flag:
+            list_datetime = list_datetime[i:]
             break
     return str(res)
 
+
 # Hàm xuất file excel
-def output_Excel(input_detail, path):
-  #Xác định số hàng và cột lớn nhất trong file excel cần tạo
-  row = len(input_detail)
-  column = len(input_detail[0])
+def output_excel(input_detail, path):
+    # Xác định số hàng và cột lớn nhất trong file excel cần tạo
+    row = len(input_detail)
+    column = len(input_detail[0])
 
-  #Tạo một workbook mới và active nó
-  wb = openpyxl.Workbook()
-  ws = wb.active
-  
-  #Dùng vòng lặp for để ghi nội dung từ input_detail vào file Excel
-  for i in range(0,row):
-    for j in range(0,column):
+    # Tạo một workbook mới và active nó
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    # Dùng vòng lặp for để ghi nội dung từ input_detail vào file Excel
+    for i in range(0, row):
+        for j in range(0, column):
+            try:
+                v = input_detail[i][j]
+                ws.cell(column=j + 1, row=i + 1, value=v)
+            except:
+                break
+
+    # Lưu lại file Excel
+    wb.save(path)
+
+
+# Hàm main
+def main():
+    global list_datetime
+
+    # Mở file config
+    f = open("config.json", encoding="utf-8")
+
+    # Trả về dạng dictionary
+    config = json.load(f)
+
+    # Lặp và kết nối các máy chấm công từ file config
+    attendances = []
+    conn = None
+
+    for x in config["time_attendance"]:
+        zk = ZK(x["ip"], port=x["port"])
         try:
-            v=input_detail[i][j]
-            ws.cell(column=j+1, row=i+1, value=v)
+            conn = zk.connect()
+            attendances += conn.get_attendance()
         except:
-            break
+            print("Kiểm tra lại máy chấm công số seri:", x["id"])
+            exit()
 
-  #Lưu lại file Excel
-  wb.save(path)
+    # Lấy thông tin tất cả người dùng trong máy
+    users = conn.get_users()
 
-# Tiêu đề excel
-header = [["AC-No.", "No.", "Name", "Date", "Timetable", "On duty", "Off duty", "Clock In", "Clock Out", "Normal", "Real time", "Late", "Early", "Absent", "OT Time", "Work Time", "Exception", "Must C/In", "Must C/Out", "Department", "NDays", "WeekEnd", "Holiday", "ATT_Time", "NDays_OT", "WeekEnd_OT", "Holiday_OT"]]
-excel = header
-# Đưa vào thông tin muốn lấy ngày chấm công
-date_from = datetime.datetime(2023, 2, 27)
-date_to = datetime.datetime(2023, 3, 1)
-date = get_date()
+    # Lấy số phút cần trừ ra trước giờ chấm công
+    minute_must_ci = config["minute"]
 
-# Lặp lấy thông tin và đưa vào mảng
-for i in range(len(users)):
-    ac_no = users[i].user_id
-    no = users[i].user_id
-    name = users[i].name
-    list_datetime_ = push_data_into_array(ac_no)
+    # Lấy on duty, off duty
+    list_on_duty = []
+    list_off_duty = []
+    list_timetable = []
 
-    for j in range(len(date)):
-        current = date[j]
+    for shift in config["shift"]:
+        list_on_duty.append(shift["work_start"])
+        list_off_duty.append(shift["work_end"])
+        list_timetable.append(shift["shift_code"])
 
-        for k in range(len(list_on_duty)):
-            row = []
-            list_datetime = list_datetime_
-            on_duty = list_on_duty[k]
-            off_duty = list_off_duty[k]
-            time_table = check_sunday(current)
-            clock_in = get_clock_in(current)
-            clock_out = get_clock_out(current) if clock_in != None else None
-         
-            row.append(int(ac_no))
-            row.append(no)
-            row.append(name)
-            row.append(current.strftime("%d/%m/%Y"))
-            row.append(time_table)
-            row.append(on_duty)
-            row.append(off_duty)
-            row.append(clock_in)
-            row.append(clock_out)
+    # Đóng file config
+    f.close()
 
-            excel.append(row)
+    # Tiêu đề excel
+    header = [["AC-No.", "No.", "Name", "Date", "Timetable", "On duty", "Off duty", "Clock In", "Clock Out", "Normal",
+               "Real time", "Late", "Early", "Absent", "OT Time", "Work Time", "Exception", "Must C/In", "Must C/Out",
+               "Department", "NDays", "WeekEnd", "Holiday", "ATT_Time", "NDays_OT", "WeekEnd_OT", "Holiday_OT"]]
+    excel = header
 
-# Đường dẫn xuất file
-path = 'D:/DuLieuChamCong.xlsx'
-# Xuất file
-output_Excel(excel, path)
-print("Xuất file Excel thành công")
+    # Đưa vào khoảng thời gian muốn lấy ngày chấm công
+    date_from = datetime.datetime(2023, 2, 27)
+    date_to = datetime.datetime(2023, 3, 1)
+    date = get_date(date_from, date_to)
 
+    # Lặp lấy thông tin và đưa vào mảng
+    for i in range(len(users)):
+        ac_no = users[i].user_id
+        no = users[i].user_id
+        name = users[i].name
+        list_datetime_ = get_attendances_of_user(ac_no, attendances, date_from, date_to)
+
+        for j in range(len(date)):
+            current = date[j]
+
+            for k in range(len(list_on_duty)):
+                row = []
+                list_datetime = list_datetime_
+                on_duty = list_on_duty[k]
+                off_duty = list_off_duty[k]
+                time_table = list_timetable[k]
+                clock_in = get_clock_in(current, on_duty, off_duty, minute_must_ci)
+                clock_out = get_clock_out(current, on_duty, minute_must_ci) if clock_in is not None else None
+
+                row.append(ac_no)
+                row.append(no)
+                row.append(name)
+                row.append(current.strftime("%d/%m/%Y"))
+                row.append(time_table)
+                row.append(on_duty)
+                row.append(off_duty)
+                row.append(clock_in)
+                row.append(clock_out)
+
+                excel.append(row)
+
+    # Đường dẫn xuất file
+    path = 'DuLieuChamCong.xlsx'
+
+    # Xuất file
+    output_excel(excel, path)
+    print("Xuất file Excel thành công")
+
+
+main()
